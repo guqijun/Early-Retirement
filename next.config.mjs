c6fc23b5aa12b92d557f5bb44ae3cdb8e3c450b1
unknown <919777829@qq.com>
2025-02-18 02:52:39 +0800

    feat: add repo and update docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const repo = "Early-Retirement";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
+    basePath,
+    assetPrefix,
     output: 'export'
 };
 
